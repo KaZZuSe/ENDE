b72112ab485cf52cbda402778dd50a1ddfcba0f5
KaZZuSe <132385139+KaZZuSe@users.noreply.github.com>
2023-05-10 16:38:03 +0200

    Add files via upload

---
+++ operaciones_use.py
@@ -0,0 +1,12 @@
+def operacion_and(numero1, numero2):
+    return f'El resultado AND es: {numero1 & numero2}'
+def operacion_or(numero1,numero2):
+    return f'El resultado OR es: {numero1 | numero2}'
+def operacion_xor(numero1,numero2):
+    return f'El resultado XOR es: {numero1 ^ numero2}'
+def operacion_not(numero1):
+    return f'El resultado NOT es: {~numero1}'
+def desplazamiento_right(numero1,numero2):
+    return f'El resultado de la operacion shift right es: {numero1>>numero2}'
+def desplazamiento_left(numero1,numero2):
+    return f'El resultado de la operacion shift left es: {numero1<<numero2}'
