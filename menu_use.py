cb47bffba4f351c12eca8cd9fa919cf2de673b93
KaZZuSe <132385139+KaZZuSe@users.noreply.github.com>
2023-05-10 16:41:31 +0200

    DONE

---
+++ menu_use.py
@@ -0,0 +1,7 @@
+from operaciones_use import operacion_and as o_and, operacion_or as o_or, operacion_xor as o_xor, operacion_not as o_not, desplazamiento_left as d_left, desplazamiento_right as d_right
+print(o_and(15,14))
+print(o_or(15,14))
+print(o_xor(15,14))
+print(o_not(15))
+print(d_left(15,14))
+print(d_right(15,14))
